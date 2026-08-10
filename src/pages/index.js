f1685363161db33d5c4ed6cc4374cbc2bd04ced6
Anjah Venter <Anjah@Anjahs-MacBook-Air.local>
2026-08-10 14:35:10 +0700

    Fix landing and shopping images

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,19 +4,21 @@ import styles from './index.module.css';
 
 export default function LandingPage() {
   return (
-    <Layout title="The Hanoi Handbook">
+    <Layout title="The Dewey Schools Hanoi Handbook">
       <main className={styles.landing}>
         <div className={styles.landingContent}>
+
           <img
-             src="/img/logo.png"
-             alt="The Dewey Schools"
-             className={styles.landingLogo}
-/>
+            src="/the-dewey-hanoi-handbook/img/hanoi-hero.png"
+            alt="The Dewey Schools"
+            className={styles.landingLogo}
+          />
+
           <h1 className={styles.landingTitle}>
-  The Dewey School
-  <br />
-  Hanoi Handbook
-</h1>
+            The Dewey Schools
+            <br />
+            Hanoi Handbook
+          </h1>
 
           <p className={styles.landingSubtitle}>
             Everything you need to live, work and thrive in Hanoi.
@@ -25,6 +27,7 @@ export default function LandingPage() {
           <Link className={styles.landingButton} to="/home">
             Let's get Started
           </Link>
+
         </div>
       </main>
     </Layout>
